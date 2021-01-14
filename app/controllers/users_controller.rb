d8e79de8240a7e8b89e8c5dd2a003d3caa2d5d49
Arpita-22 <duttaarpita22@gmail.com>
2021-01-14 12:07:30 -0800

    refactored the code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
     skip_before_action :fetch_user, only: [:new,:create]
-    # def index
-    #    @users = User.all
-    # end
-
     def show
         @user = User.find(params[:id])
     end
